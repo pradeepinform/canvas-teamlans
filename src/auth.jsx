9d906502c8cf6ae655b372d2fac7bf292de59a1d
Pradeep Kumar <pradeepinform12@gmail.com>
2025-06-11 14:21:51 +0530

    frontend added

--- src/auth.jsx
+++ src/auth.jsx
@@ -2,19 +2,30 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
   callbacks: {
     async jwt({ token, account }) {
       if (account?.id_token) {
         token.idToken = account.id_token;
       }
-
       return token;
     },
     async session({ session, token }) {
       session.idToken = token.idToken;
-      //console.log(session,"session")
       return session;
     },
   },
 });
+  
